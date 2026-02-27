b2fea3189bb34469d93e65a01afe918d5cb52446
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2026-02-28 00:20:22 +0530

    Add files via upload
    Added more lecture

---
+++ Java/Lecture3/src/VariablesDatatypes.java
@@ -0,0 +1,13 @@
+public class VariablesDatatypes {
+    public static void main(String[] args) {
+//        Integer   -> byte,short,int,long
+        byte firstNum = 11;
+        short secondNum = 12;
+        int thirdNum = 13;
+        long fourthNum = 15l;
+
+        System.out.println("Integer Num: byte: " + firstNum + " short: " + secondNum + " int: " + thirdNum + " long: " + fourthNum);
+
+//        real
+    }
+}
